b24782ec8f94525f2f442e30e9182324f1aac3bd
Palak-SweetCoder <pspalak6@gmail.com>
2023-01-11 19:53:27 +0600

    mongodb connected getting all data from the server

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middleware
+// -----middleware-----
 app.use(cors());
 app.use(express.json());
+
+// -------mongodb connection---------
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pwfkf3v.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+// --------------------CRUD FUNCTION START----------------------
+async function run() {
+    try {
+        // console.log('db connected successfully!!!');
+        const partsCollection = client.db('pcxMaterial').collection('parts');
+
+        // API TO: Get or Read all data from the database
+        app.get('/parts', async (req, res) => {
+            const query = {};
+            const cursor = partsCollection.find(query);
+            const parts = await cursor.toArray();
+            res.send(parts);
+        });
+    } finally {
+        // client.close();
+    }
+}
+run().catch(console.dir);
+// --------------------CRUD FUNCTION END----------------------
 
 app.get('/', (req, res) => {
     res.send('Hello From PCX Server!');
